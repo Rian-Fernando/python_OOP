3bc26b82a794980102e984d4a20a9d33649b1dec
Rian Fernando <rian.fernando2006@gmail.com>
2024-06-15 11:53:35 +0530

    Create Snake without Movement

--- mygame.py
+++ mygame.py
@@ -1,6 +1,16 @@
 import pygame, sys, random
 from pygame.math import Vector2
 
+class SNAKE:
+    def __init__(self):
+        self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
+
+    def draw_snake(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            pygame.draw.rect(screen, (183, 111, 122), block_rect)
 class FRUIT:
     def __init__(self):
         self.x = random.randint(0, cell_number - 1)
@@ -11,7 +21,6 @@ class FRUIT:
         fruit_rect = pygame.Rect(int(self.pos.x * cell_size), int(self.pos.y * cell_size), cell_size, cell_size)
         pygame.draw.rect(screen, (126, 166, 114), fruit_rect)
         
-
 pygame.init()
 cell_size = 40
 cell_number = 20
@@ -19,6 +28,7 @@ screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_numb
 clock = pygame.time.Clock()
 
 fruit = FRUIT()
+snake = SNAKE()
 
 while True:
     for event in pygame.event.get():
@@ -28,5 +38,6 @@ while True:
 
     screen.fill((175, 215, 70))
     fruit.draw_fruit()
+    snake.draw_snake()
     pygame.display.update()
     clock.tick(60)
