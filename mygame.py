9c4fb1924e32dfefd31f705b2ea8b65e397bf162
Rian Fernando <rian.fernando2006@gmail.com>
2024-06-20 01:33:42 +0530

    Fixed Snake Reset Issue

--- mygame.py
+++ mygame.py
@@ -4,7 +4,7 @@ from pygame.math import Vector2
 class SNAKE:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
-        self.direction = Vector2(1, 0)
+        self.direction = Vector2(0, 0)
         self.new_block = False
 
         self.head_up = pygame.image.load("Graphics/head_up.png").convert_alpha()
@@ -99,6 +99,11 @@ class SNAKE:
 
     def  play_crunch_sound(self):
         self.crunch_sound.play()
+
+    def reset(self):
+        self.body = [Vector2(5, 10), Vector2(4, 10), Vector2(3, 10)]
+        self.direction = Vector2(0, 0)
+
 class FRUIT:
     def __init__(self):
         self.x = random.randint(0, cell_number - 1)
@@ -150,9 +155,7 @@ class MAIN:
                 self.game_over()
 
     def game_over(self):
-        pygame.quit()
-        sys.exit()
-
+        self.snake.reset()
     def draw_grass(self):
         grass_color = (167, 209, 61)
 
